39090efa61b16ab646a4577f6f7dcb7e462d6e57
Francisco Montejo <montejof1998@gmail.com>
2024-08-10 15:27:33 -0700

    Creating the middlewares for the errors, and validation

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import Express, { json } from "express";
-import {routerApi} from "../routes/index.js";
+import {routerApi} from "./routes/index.js";
+import { errorHandler, boomErrorHandler } from "./middlewares/errorHandler.js";
 
 const app = Express();
 const port = 5133;
@@ -10,6 +11,8 @@ app.get('/api', (req, res) => {
 })
 
 routerApi(app)
+app.use(boomErrorHandler)
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
