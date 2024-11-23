258151c9f5f611496f4d3ceaee968d0bfb1e6f04
Francisco Montejo <montejof1998@gmail.com>
2024-11-22 17:34:07 -0800

    users service done

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,14 @@
 import  Express from "express";
 import {router as postsRouter} from "./postsRouter.js"
 import {router as bookmarksRouter} from "./bookmarksRouter.js"
+import {router as usersRouter} from "./usersRouter.js"
 
 function routerApi(app){
     const router = Express.Router()
     app.use("/api/v1", router)
     router.use("/posts", postsRouter)
     router.use("/bookmarks", bookmarksRouter)
+    router.use("/users", usersRouter)
 }
 
 export {routerApi}
