f946b05dab76274629292f15768828648e4712a3
Francisco Montejo <montejof1998@gmail.com>
2025-06-18 19:22:08 -0700

    New variable

--- api/schemas/usersSchema.js
+++ api/schemas/usersSchema.js
@@ -3,6 +3,9 @@ const Joi = require("joi");
 const userName = Joi.string().alphanum();
 const email = Joi.string().email();
 const password = Joi.string().alphanum();
+const id = Joi.string().guid({ version: "uuidv4" });
+
+
 
 const createUserSchema = Joi.object({
   email: email.required(),
