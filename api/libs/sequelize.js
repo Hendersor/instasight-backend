bf6b286f771974a31b9719246df6b77e6cd890e5
Francisco Montejo <montejof1998@gmail.com>
2025-06-18 19:11:48 -0700

    added findUserSchema

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -3,6 +3,8 @@ const { config } = require("../config/config.js");
 const { setupModels } = require("../db/models/index.js");
 
 const isProduction = config.env === 'production';
+console.log(isProduction)
+
 const databaseUrl = isProduction ? config.dbProd : config.dbDev;
 
 console.log(databaseUrl)
